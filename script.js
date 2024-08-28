32cb3e0da304b09205d9eda253d227c5010dc78a
Artemka1806 <74206792+Artemka1806@users.noreply.github.com>
2024-08-29 00:19:36 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,132 @@
+const tabContainer = document.querySelector(".tab-container");
+let tabChildrens = [];
+
+
+let data = {}
+
+axios.get("data.json")
+.then(function (response) {
+	const data = response.data
+	const grades = Object.keys(data);
+	let activeTab = grades[0]
+
+	createTabs(data, grades, activeTab)
+	createStudentsTable(data, grades)
+	document.querySelector(`#grade-${activeTab}`).classList.remove("is-hidden")
+})
+
+
+const statuses = [
+	["si-check", "green;"],
+	["si-location", "pink"],
+	["si-clock", "blue;"],
+	["si-x", "red"],
+]
+
+function createTabs(data, grades, activeTab) {
+	grades.forEach(function(e, i) {
+		let tab = document.createElement("a")
+		tab.innerText = `${e}-ті класи`
+		tab.setAttribute("id", e)
+		tab.classList.add("tab", "has-text-center", "has-h-8", "navlink")
+		tab.setAttribute("style", "padding-top: 5px; padding-bottom: 0px;")
+		if (e == activeTab) {
+			tab.classList.add("active", "has-bg-white", "has-text-primary")
+		}
+		tab.addEventListener("click",  function() { setTab(tab, tabChildrens, e); }, false)
+		tabChildrens.push(tab)
+		tabContainer.appendChild(tab)
+
+	});
+}
+
+
+function setTab(target, allElements, grade) {
+	if (!target.classList.contains("active")){
+		allElements.forEach(function(e) {
+			if (e.classList.contains("active")) {
+				e.classList.remove("active", "has-bg-white", "has-text-primary")
+			}
+		});
+		target.classList.add("active", "has-bg-white", "has-text-primary")
+		grades.forEach(function(element, i) {
+			block = document.querySelector(`#grade-${element}`)
+			if (!block.classList.contains("is-hidden")) {
+				block.classList.add("is-hidden");
+			} else {
+				block.classList.remove("is-hidden");
+			}
+		});
+	}
+
+}
+
+
+function createStudentsTable(data, grades) {
+	grades.forEach(function(grade) {
+		let section = createSection(grade)
+		let gradeData = data[grade]
+		let groupNames = Object.keys(gradeData)
+		groupNames.forEach(function(group) {
+			let table = createTable(group)
+			let tbody = table.querySelector("tbody")
+			let studentsData = gradeData[group]
+			let studentNames = Object.keys(studentsData)
+			studentNames.forEach(function(student) {
+				let status = studentsData[student]
+				let row = createRow(student, status)
+
+				tbody.appendChild(row)
+			});
+			section.appendChild(table)
+		});
+		document.querySelector("body").appendChild(section)
+	});
+}
+
+function createSection(grade) {
+	let section = document.createElement("div")
+	section.setAttribute("id", `grade-${grade}`)
+	section.classList.add("table-container", "columns", "is-hidden", "has-w-full")
+	return section
+}
+
+
+function createTable(groupName) {
+	let table = document.createElement("table")
+	table.classList.add("column", "is-full-mobile", "has-mb-0")
+	table.setAttribute("style", "min-width: 20% !important; max-width: 20% !important")
+	let head = document.createElement("thead")
+	let tr = document.createElement("tr")
+	let thName = document.createElement("th")
+	thName.classList.add("has-p-2")
+	thName.innerText = groupName
+	let thStatus = document.createElement("th")
+	thStatus.classList.add("has-p-2")
+	thStatus.innerText = "Статус"
+	tr.appendChild(thName)
+	tr.appendChild(thStatus)
+	head.appendChild(tr)
+	table.appendChild(head)
+	let body = document.createElement("tbody")
+	table.appendChild(body)
+	return table
+}
+
+
+function createRow(name, status) {
+	let row = document.createElement("tr")
+	let tdName = document.createElement("td")
+	tdName.classList.add("has-p-2")
+	tdName.innerText = name
+	let tdStatus = document.createElement("td")
+	tdStatus.classList.add("has-p-2", "has-text-center")
+	let statusIcon = document.createElement("i")
+	statusIcon.classList.add(statuses[status][0])
+	statusIcon.setAttribute("style", `font-size: 20px; color: ${statuses[status][1]};`)
+	tdStatus.appendChild(statusIcon)
+	row.appendChild(tdName)
+	row.appendChild(tdStatus)
+	return row
+}
+

b32179282259169759773f7f8eb35d23ff9a2e33
Artemka1806 <tyschenkoartem2580@gmail.com>
2024-09-19 20:04:31 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+  function a() {
+    alert("a")
+  }
+</script>
+
+<template>
+  <button class="settings-button" @click="a">
+    <i class="si-hamburger"></i>
+  </button>
+</template>

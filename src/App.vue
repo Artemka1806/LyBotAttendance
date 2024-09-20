25ed78ce3fab189e0ec0db29f48febb9c5778741
Artemka1806 <tyschenkoartem2580@gmail.com>
2024-09-20 13:03:36 +0300

    Refactor code to remove unused dependencies and API call

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <script setup>
 import { ref, reactive } from 'vue'
+import axios from 'axios'
+
+let grades = ref([])
+
+axios.get("https://lybotapi.onrender.com/attendance")
+  .then(response => {
+    grades.value = Object.keys(response.data)
+    console.log(grades)
+    grades.value.forEach(grade => {
+      tabs.value.push({
+        grade: grade,
+        isActive: false
+      })
+    })
+
+    setActiveTab(tabs.value[0].grade)
+})
+
 
 function a() {
   alert("a")
 }
 
-let grades = ref([10, 11, 12])
+
 let tabs = ref([])
-grades.value.forEach(grade => {
-  tabs.value.push({
-    grade: grade,
-    isActive: false
-  })
-})
+
 
 const setActiveTab = (grade) => {
   tabs.value.forEach(tab => {
     tab.isActive = tab.grade === grade
   })
 }
-
-setActiveTab(tabs.value[0].grade)
 
 const activeClass = reactive({
   "active": true,
@@ -39,7 +50,8 @@ const getTabClass = (grade) => {
     <i class="si-hamburger"></i>
   </button>
   <div class="tab-container is-flex has-direction-column-mobile has-bg-muted has-p-1">
-    <a v-for="tab in tabs" :key="tab.grade" @click="setActiveTab(tab.grade)" :id="tab.grade" class="tab has-text-center has-h-8 navlink" :class="getTabClass(tab.grade)">
+    <a v-for="tab in tabs" :key="tab.grade" @click="setActiveTab(tab.grade)" :id="tab.grade"
+      class="tab has-text-center has-h-8 navlink" :class="getTabClass(tab.grade)">
       {{ tab.grade + "-ті класи" }}
     </a>
   </div>
